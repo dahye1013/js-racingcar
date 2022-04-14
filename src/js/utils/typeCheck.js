f54861d9a81b9984b399b7851d014734dbc09cf9
dahye1013 <alrean@naver.com>
2022-04-14 16:01:17 +0900

    refactor: instanceType 인자명 clazz로 수정

--- src/js/utils/typeCheck.js
+++ src/js/utils/typeCheck.js
@@ -1,4 +1,4 @@
-export const instanceCheck = (instance, instanceType) => {
-  if (!(instance instanceof instanceType))
+export const instanceCheck = (instance, clazz) => {
+  if (!(instance instanceof clazz))
     throw Error('해당하는 인스턴스의 인자만 받을 수 있습니다.');
 };
